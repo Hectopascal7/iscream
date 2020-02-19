2eb7b2ed2067af5a4972931886f78bfe688bd9bb
Hectopascal7 <hectopascal7@yeah.net>
2020-02-19 23:09:12 +0800

    项目初始化。

---
+++ src/main/java/cn/net/iscream/iscream/IscreamApplication.java
@@ -0,0 +1,13 @@
+package cn.net.iscream.iscream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/net/iscream/iscream/IscreamApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.iscream.iscream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
